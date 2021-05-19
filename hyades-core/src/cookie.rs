e49932796980cf2d44b05d7048e86e5fb9d94a4d
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-05-20 00:29:40 +0530

    add conversions to/from Vec<u8>

--- hyades-core/src/cookie.rs
+++ hyades-core/src/cookie.rs
@@ -1,7 +1,48 @@
-struct Cookie {}
+use rand::{thread_rng, Rng};
+
+#[derive(Clone, Debug)]
+pub struct Cookie {
+    internal: Vec<u8>,
+}
 
 impl Cookie {
     pub fn new() -> Self {
-        todo!()
+        let mut buf = [0u8; 4];
+        thread_rng().fill(&mut buf);
+
+        Self {
+            internal: buf.to_vec(),
+        }
     }
 }
+
+impl From<Cookie> for Vec<u8> {
+    fn from(cookie: Cookie) -> Self {
+        cookie.internal
+    }
+}
+
+impl From<Vec<u8>> for Cookie {
+    fn from(buf: Vec<u8>) -> Self {
+        Self {
+            internal: buf
+        }
+    }
+}
+
+impl From<&Vec<u8>> for Cookie {
+    fn from(buf: &Vec<u8>) -> Self {
+        Self {
+            internal: buf.clone()
+        }
+    }
+}
+
+
+/*
+pub fn gen_cookie() {
+    let rng = rand::SystemRandom::new();
+
+    let key = hmac::Key::generate(hmac::HMAC_SHA256, &rng)?;
+}
+*/

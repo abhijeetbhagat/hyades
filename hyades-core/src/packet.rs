3e028f9927efe1b46c792233a6a5e5abdcfea91b
abhijeetbhagat <abhijeet.bhagat@gmx.com>
2021-07-08 21:34:30 +0530

    wip: convert raw bytes to an sctp packet

--- hyades-core/src/packet.rs
+++ hyades-core/src/packet.rs
@@ -1,5 +1,19 @@
-use crate::chunk::Chunk;
+use crate::chunk::{Chunk, Init, InitAck};
+use crate::error::SCTPError;
 use crc32c;
+use std::convert::TryFrom;
+
+/*
+        0                   1                   2                   3
+        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |     Source Port Number        |     Destination Port Number   |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |                      Verification Tag                         |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |                           Checksum                            |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+*/
 
 pub struct CommonHeader {
     src_port: u16,
@@ -19,6 +33,20 @@ impl Default for CommonHeader {
     }
 }
 
+/*
+        0                   1                   2                   3
+        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |                        Common Header                          |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |                          Chunk #1                             |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |                           ...                                 |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+       |                          Chunk #n                             |
+       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+*/
+
 /// An SCTP Packet
 pub struct Packet {
     header: CommonHeader,
@@ -32,6 +60,45 @@ impl From<&Packet> for Vec<u8> {
             v.extend(Vec::<u8>::from(chunk));
         }
         v
+    }
+}
+
+impl TryFrom<Vec<u8>> for Packet {
+    type Error = SCTPError;
+
+    fn try_from(raw_data: Vec<u8>) -> Result<Self, Self::Error> {
+        // TODO abhi: convert raw bytes into a packet
+        let header = CommonHeader {
+            src_port: u16::from_be_bytes(<[u8; 2]>::try_from(&raw_data[0..2]).unwrap()),
+            dst_port: u16::from_be_bytes(<[u8; 2]>::try_from(&raw_data[2..4]).unwrap()),
+            ver_tag: u32::from_be_bytes(<[u8; 4]>::try_from(&raw_data[4..8]).unwrap()),
+            checksum: u32::from_be_bytes(<[u8; 4]>::try_from(&raw_data[8..12]).unwrap()),
+        };
+
+        let mut offset = 12usize;
+        let mut chunks: Vec<Box<dyn Chunk>> = vec![];
+
+        while offset < raw_data.len() {
+            match raw_data[offset] {
+                1 => {
+                    let len = u16::from_be_bytes(
+                        <[u8; 2]>::try_from(&raw_data[offset + 3..=offset + 4]).unwrap(),
+                    ) as usize;
+                    chunks.push(Box::new(Init::from(&raw_data[offset..len])));
+                    offset += len;
+                }
+                2 => {
+                    let len = u16::from_be_bytes(
+                        <[u8; 2]>::try_from(&raw_data[offset + 3..=offset + 4]).unwrap(),
+                    ) as usize;
+                    chunks.push(Box::new(InitAck::from(&raw_data[offset..len])));
+                    offset += len;
+                }
+                _ => return Err(SCTPError::InvalidSCTPPacket),
+            }
+        }
+
+        Ok(Packet { header, chunks })
     }
 }
 
